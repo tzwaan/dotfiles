c96bb840e25deb41c6dedc501285ddcc828133ab
Tijmen Zwaan@Server-118 <tijmen.zwaan@gmail.com>
2019-07-11 20:41:24 -0400

    add bash git and tmux config and add setup

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BACKUP_DIR=$PWD/backup
+ORIGINAL_BASHRC=$HOME/.bashrc
+ORIGINAL_VIMRC=$HOME/.vimrc
+ORIGINAL_FISHCONF_FOLDER=$HOME/.config/fish/
+ORIGINAL_GITCONF=@HOME/.gitconfig
+
+# Create vim folders
+mkdir -p ~/.vim/{autoload,colors}
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+
+
+# Install vim-plug plugin manager
+wget -O ~/.config/nvim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+ln -sf $PWD/bash/bashrc $ORIGINAL_BASHRC
+ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
+ln -sf $PWD/vim/vimrc $HOME/.vim/init.vim
+ln -sf $PWD/fish/* $ORIGINAL_FISHCONF_FOLDER
+ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONF
