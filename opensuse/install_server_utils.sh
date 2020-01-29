4033b69dc77487c33355cb6196ebf241c291d2ad
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2020-01-29 03:11:18 +0100

    Add moreutils to server install script

--- opensuse/install_server_utils.sh
+++ opensuse/install_server_utils.sh
@@ -6,7 +6,8 @@ sudo zypper install -y vim git python-pip python3-pip\
 	htop python-devel python3-devel tig curl fish\
 	tmux whois wget neovim cmake pyenv\
 	zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel\
-	readline-devel sqlite3 sqlite3-devel xz xz-devel
+	readline-devel sqlite3 sqlite3-devel xz xz-devel\
+    moreutils
 
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 
