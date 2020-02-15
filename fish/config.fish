49d80ba41206cfdf38349839f6175722db34e502
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2020-02-15 18:13:19 +0100

    Rename nvim to vim if it's available

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,16 @@ function sudo
 		command sudo $argv
 	end
 end
+
+
+function vim
+    if command -s nvim > /dev/null
+        nvim $argv
+    else
+        command vim $argv
+    end
+end
+
+function ovim
+    command vim $argv
+end
